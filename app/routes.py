e1a7fe697c4a71fd8e2828943cbb35647269e923
ManOAction <7423159+ManOAction@users.noreply.github.com>
2022-08-18 19:35:16 -0600

    Bootstrap implementation

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from app.models import Player
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Jacob'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -27,3 +29,10 @@ def login():
             form.username.data, form.remember_me.data))
         redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/players')
+def players():
+    user = {'username': 'Jacob'}
+    players = Player.query.all()
+    return render_template('players.html', title='Players', user=user, players=players)
